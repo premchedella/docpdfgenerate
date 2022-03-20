21933bec271c3a43dc4be14db13ff36d8538c3d4
Prem Kumar Chedella <prem.chedella@gmail.com>
2022-03-20 11:54:26 +0300

    Doc2Pdf:main: Added Command line parameters support.

--- src/doctopdf.cc
+++ src/doctopdf.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 
 #include <QtCore/QCoreApplication>
+#include <QtCore/QString>
 
 using namespace std;
 
@@ -10,7 +11,25 @@ int main(int argc, char *argv[])
 
   std::cout << "Convert Document into different PDF files." << std::endl;
 
-  std::cout << "No. of Arguments = " << argc << std::endl;
+  int no_args = argc;
+
+  std::cout << "No. of Arguments = " << no_args << std::endl;
+
+  if (no_args == 3)
+  {
+    QString input_file_name = QString(argv[1]);
+
+    std::cout << "Input File Name = " << input_file_name.toStdString() 
+        << std::endl;
+
+    QString output_dir = QString(argv[2]);
+
+    std::cout << "Output Path = " << output_dir.toStdString() << std::endl;
+
+  } else
+  {
+    std::cout << "Not valid arguments." << std::endl; 
+  }
 
   core_app.exit();
 }
