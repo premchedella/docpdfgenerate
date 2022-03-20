1cd1e9cd5c68d20385c0795bd0480147fa4336eb
Prem Kumar Chedella <prem.chedella@gmail.com>
2022-03-20 11:19:33 +0300

    Doc2Pdf:Added main file.

---
+++ src/doctopdf.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <QtCore/QCoreApplication>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  QCoreApplication a(argc, argv);
+
+  std::cout << "Convert Document into different PDF files." << std::endl;
+
+  return 0;
+}
