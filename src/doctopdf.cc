1941be72fe754013a080cfea53a5127c00ed55c7
Prem Kumar Chedella <prem.chedella@gmail.com>
2022-03-21 09:24:56 +0300

    Doc2Pdf:main: Add more command line parameters.

--- src/doctopdf.cc
+++ src/doctopdf.cc
@@ -15,17 +15,23 @@ int main(int argc, char *argv[])
 
   std::cout << "No. of Arguments = " << no_args << std::endl;
 
-  if (no_args == 3)
+  if (no_args == 5)
   {
-    QString input_file_name = QString(argv[1]);
+    bool is_process = true;
 
-    std::cout << "Input File Name = " << input_file_name.toStdString() 
-        << std::endl;
+    QString data_file = QString(argv[1]);
+    std::cout << "Data File = " << data_file.toStdString() << std::endl;
 
-    QString output_dir = QString(argv[2]);
 
+
+    QString template_file = QString(argv[2]);
+    std::cout << "Template File = " << template_file.toStdString() << std::endl;
+
+    QString replace_file = QString(argv[3]);
+    std::cout << "Replacement File = " << replace_file.toStdString() << std::endl;
+
+    QString output_dir = QString(argv[4]);
     std::cout << "Output Path = " << output_dir.toStdString() << std::endl;
-
   } else
   {
     std::cout << "Not valid arguments." << std::endl; 
