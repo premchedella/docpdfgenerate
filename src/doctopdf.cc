d45f063abb4bf7cb68225a44e7d0cd1fa7f4cfd9
Prem Kumar Chedella <prem.chedella@gmail.com>
2022-03-21 10:34:09 +0300

    Doc2Pdf:main: Validate Data and Template files.

--- src/doctopdf.cc
+++ src/doctopdf.cc
@@ -2,8 +2,14 @@
 
 #include <QtCore/QCoreApplication>
 #include <QtCore/QString>
+#include <QtCore/QFileInfo>
 
 using namespace std;
+
+bool IsValidDataFile(QString file_name);
+bool IsValidTemplateFile(QString file_name);
+bool IsValidReplaceFile(QString file_name);
+bool IsValidOutputPath(QString dir_path);
 
 int main(int argc, char *argv[])
 {
@@ -19,13 +25,22 @@ int main(int argc, char *argv[])
   {
     bool is_process = true;
 
+    // Get the Data file
     QString data_file = QString(argv[1]);
     std::cout << "Data File = " << data_file.toStdString() << std::endl;
 
-
+    if (!IsValidDataFile(data_file))
+    {
+      is_process = false;
+    }
 
     QString template_file = QString(argv[2]);
     std::cout << "Template File = " << template_file.toStdString() << std::endl;
+
+    if (!IsValidTemplateFile(template_file))
+    {
+      is_process = false;
+    }
 
     QString replace_file = QString(argv[3]);
     std::cout << "Replacement File = " << replace_file.toStdString() << std::endl;
@@ -38,4 +53,75 @@ int main(int argc, char *argv[])
   }
 
   core_app.exit();
+}
+
+
+bool IsValidDataFile(QString file_name)
+{
+  bool is_valid = true;
+
+  //Check whether Data file exists or not
+  QFileInfo fi(file_name);
+
+  if (fi.exists())
+  {
+    //Get the Extension of file
+
+    QString ext = fi.suffix();
+    if (ext.compare("csv") != 0)
+    {
+      is_valid = false;
+      std::cout << "Data file extension is not in csv file format." << std::endl;
+    }    
+  } else
+  {
+    is_valid = false;
+    std::cout << "Data file doest not exists." << std::endl;
+  }
+
+  return is_valid;
+}
+
+bool IsValidTemplateFile(QString file_name)
+{
+  bool is_valid = false;
+  //Check whether Data file exists or not
+  QFileInfo fi(file_name);
+
+  if (fi.exists())
+  {
+    is_valid = true;
+    //Get the Extension of file
+
+    QString ext = fi.suffix();
+    if ((ext.compare("doc") == 0) || (ext.compare("docx") == 0))
+    {
+      is_valid = true;      
+    } else
+    {
+      is_valid = false;
+      std::cout << "Template file extension is not in correct format." << std::endl;
+    }
+
+  } else
+  {
+    is_valid = false;
+    std::cout << "Template file doest not exists." << std::endl;
+  }
+
+  return is_valid;
+}
+
+bool IsValidReplaceFile(QString file_name)
+{
+  bool is_valid = true;
+
+  return is_valid;
+}
+
+bool IsValidOutputPath(QString dir_path)
+{
+  bool is_valid = true;
+
+  return is_valid;
 }
