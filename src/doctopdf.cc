2b8168e87bdf36cc0059992f2d7e28dd675769f3
Prem Kumar Chedella <prem.chedella@gmail.com>
2022-04-10 14:30:32 +0300

    Doc2Pdf: SW:Used Utilities class files.

--- src/doctopdf.cc
+++ src/doctopdf.cc
@@ -2,19 +2,11 @@
 
 #include <QtCore/QCoreApplication>
 #include <QtCore/QString>
-#include <QtCore/QFileInfo>
-#include <QtCore/QDir>
-#include <QtCore/QProcess>
-#include <QtCore/QSettings>
+
+#include "utilities.h"
 
 using namespace std;
 
-bool IsValidDataFile(QString file_name);
-bool IsValidTemplateFile(QString file_name);
-bool IsValidReplaceFile(QString file_name);
-bool IsValidOutputPath(QString dir_path);
-
-QString GetWordPath();
 
 int main(int argc, char *argv[])
 {
@@ -35,7 +27,7 @@ int main(int argc, char *argv[])
     std::cout << "Data File = " << data_file.toStdString() << std::endl;
 
     // Validate the Data file
-    if (!IsValidDataFile(data_file))
+    if (!Utilities::IsValidDataFile(data_file))
     {
       is_process = false;
     }
@@ -45,7 +37,7 @@ int main(int argc, char *argv[])
     std::cout << "Template File = " << template_file.toStdString() << std::endl;
 
     // Validate Template file
-    if (!IsValidTemplateFile(template_file))
+    if (!Utilities::IsValidTemplateFile(template_file))
     {
       is_process = false;
     }
@@ -55,7 +47,7 @@ int main(int argc, char *argv[])
     std::cout << "Replacement File = " << replace_file.toStdString() << std::endl;
 
     // Validate Replace file
-    if (!IsValidReplaceFile(replace_file))
+    if (!Utilities::IsValidReplaceFile(replace_file))
     {
       is_process = false;
     }
@@ -65,7 +57,7 @@ int main(int argc, char *argv[])
     std::cout << "Output Path = " << output_dir.toStdString() << std::endl;
 
     // Validate the Output Directory
-    if (!IsValidOutputPath(output_dir))
+    if (!Utilities::IsValidOutputPath(output_dir))
     {
       is_process = false;
     }
@@ -74,122 +66,10 @@ int main(int argc, char *argv[])
     std::cout << "Not valid arguments." << std::endl; 
   }
 
+
+  QString word_path = Utilities::GetWordPath();
+
   core_app.exit();
 }
 
 
-bool IsValidDataFile(QString file_name)
-{
-  bool is_valid = true;  
-  QFileInfo fi(file_name);
-
-  // Check whether Data file exists or not
-  if (fi.exists())
-  {
-    // Get the Extension of file
-    QString ext = fi.suffix();
-
-    // The Data file extensions is always csv
-    if (ext.compare("csv") != 0)
-    {
-      is_valid = false;
-      std::cout << "Data file extension is not in csv file format." << std::endl;
-    }    
-  } else
-  {
-    is_valid = false;
-    std::cout << "Data file doest not exists." << std::endl;
-  }
-
-  return is_valid;
-}
-
-bool IsValidTemplateFile(QString file_name)
-{
-  bool is_valid = false;  
-  QFileInfo fi(file_name);
-
-  //Check whether Data file exists or not
-  if (fi.exists())
-  {    
-    // Get the Extension of file
-    QString ext = fi.suffix();
-
-    // The template file's extension is always doc or docx
-    if ((ext.compare("doc") == 0) || (ext.compare("docx") == 0))
-    {
-      is_valid = true;      
-    } else
-    {
-      is_valid = false;
-      std::cout << "Template file extension is not in correct format." << std::endl;
-    }
-
-  } else
-  {
-    is_valid = false;
-    std::cout << "Template file doest not exists." << std::endl;
-  }
-
-  return is_valid;
-}
-
-bool IsValidReplaceFile(QString file_name)
-{
-  bool is_valid = true;  
-  QFileInfo fi(file_name);
-
-  // Check whether Data file exists or not
-  if (fi.exists())
-  {
-    // Get the Extension of file
-    QString ext = fi.suffix();
-
-    // The replacement file extension is alwys txt
-    if (ext.compare("txt") != 0)
-    {
-      is_valid = false;
-      std::cout << "Replace file extension is not in txt file format." << std::endl;
-    }
-  } else
-  {
-    is_valid = false;
-    std::cout << "Repalce file doest not exists." << std::endl;
-  }
-
-  return is_valid;
-}
-
-bool IsValidOutputPath(QString dir_path)
-{
-  bool is_valid = false;  
-  QFileInfo dir(dir_path);
-
-  // Check whether the output directory is directory or not
-  if (dir.isDir())  
-  {
-    is_valid = true;
-  } else
-  {    
-    std::cout << "Output is not a directory." << std::endl;
-  }
-
-  return is_valid;
-}
-
-QString GetWordPath()
-{
-  QString word_path = "";
-
-  // Create Setting path variable to gt the word path
-  QString path = "HKEY_LOCAL_MACHINE/SOFTWARE/Microsoft/Windows/";
-  path += "CurrentVersion/App Paths/Winword.exe";
-
-  path = QDir::toNativeSeparators(path);  
-  QSettings settings(path, QSettings::NativeFormat);
-
-  // . is default, so use that to get the value of the default.
-  word_path = settings.value(".").toString();  
-
-  return word_path;
-}
